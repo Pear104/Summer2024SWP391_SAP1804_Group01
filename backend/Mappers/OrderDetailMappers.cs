791c3ec3ff17ddb93475a99650fc428bb24828d5
DucDao <minkendy0@gmail.com>
2024-05-25 00:42:45 +0700

    feat: add mappers

---
+++ backend/Mappers/OrderDetailMappers.cs
@@ -0,0 +1,55 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using backend.DTOs;
+using backend.Models;
+
+namespace backend.Mappers
+{
+    public static class OrderDetailMappers
+    {
+        public static CreateOrderDetailDTO ToCreateOrderDetailDTO(this OrderDetail orderDetail) 
+        { 
+            return new CreateOrderDetailDTO
+            {
+                Size = orderDetail.Size,
+                DiamondId =orderDetail.DiamondId,
+                AccessoryId = orderDetail.AccessoryId
+            };
+        }
+        public static OrderDetail ToOrderDetailFromCreate(this CreateOrderDetailDTO orderDetailDTO, int orderID) 
+        { 
+            return new OrderDetail
+            {
+                Size = orderDetailDTO.Size,
+                DiamondId = orderDetailDTO.DiamondId,
+                AccessoryId = (long)orderDetailDTO.AccessoryId,
+                OrderId = orderID
+            };
+        }
+        public static OrderDetail ToOrderDetailFromCreate(this CreateOrderDetailDTO orderDetailDTO, int orderID) 
+        { 
+            return new OrderDetail
+            {
+                Size = orderDetailDTO.Size,
+                DiamondId = orderDetailDTO.DiamondId,
+                AccessoryId = (long)orderDetailDTO.AccessoryId,
+                OrderId = orderID
+            };
+        }
+        public static OrderDetailDTO ToOrderDetailDTO(this OrderDetail orderDetail) 
+        { 
+            return new OrderDetailDTO
+            {
+                OrderDetailId = orderDetail.OrderDetailId,
+                OrderId = orderDetail.OrderId,
+                Size = orderDetail.Size,
+                DiamondId = orderDetail.DiamondId,
+                AccessoryId = orderDetail.AccessoryId,
+                ItemPrice = orderDetail.ItemPrice
+            };
+        }
+
+    }
+}
---
+++ backend/Mappers/OrderMappers.cs
@@ -0,0 +1,55 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using backend.DTOs;
+using backend.Models;
+
+namespace backend.Mappers
+{
+    public static class OrderMappers
+    {
+        public static UpdateOrderDTO ToOrderUpdate(this Order order) 
+        { 
+            return new UpdateOrderDTO
+            {
+                ConfirmStatus = order.ConfirmStatus,
+                OrderStatus = order.OrderStatus,
+                ContactStatus = order.ContactStatus,
+                DeliveryStatus = order.DeliveryStatus
+
+            };
+        }
+        public static Order ToOrderFromUpdate(this UpdateOrderDTO order) 
+        { 
+            return new Order
+            {
+                ConfirmStatus = order.ConfirmStatus,
+                OrderStatus = order.OrderStatus,
+                ContactStatus = order.ContactStatus,
+                DeliveryStatus = order.DeliveryStatus
+            };
+        }
+        public static OrderDTO ToOrderDTO(this Order order) 
+        { 
+            return new OrderDTO
+            {
+                OrderId= order.OrderId,
+                TotalPrice = order.TotalPrice,
+                TotalDiscountPercent= order.TotalDiscountPercent,
+                ContactStatus = order.ContactStatus,
+                ConfirmStatus = order.ConfirmStatus,
+                DeliveryStatus = order.DeliveryStatus,
+                OrderStatus = order.OrderStatus,
+                ShippingAddress = order.ShippingAddress,
+                CreatedAt = order.CreatedAt,
+                CustomerId = order.CustomerId,
+                SaleStaffId = order.SaleStaffId,
+                DeliveryStaffId = order.DeliveryStaffId,
+                PromotionId = order.PromotionId,
+                OrderDetails= order.OrderDetails.Select(o => o.ToOrderDetailDTO()).ToList()
+                
+            };
+        }
+    }
+}
