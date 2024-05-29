695471f4b86771c5fc12346691a871fd01162359
Dat <datntse180123@fpt.edu.vn>
2024-05-29 11:39:30 +0700

    feat: add new screen
    - accessory detail screen
    - order history screen

--- frontend/src/pages/account/OrderHistory.tsx
+++ frontend/src/pages/account/OrderHistory.tsx
@@ -1,5 +1,16 @@
 import React from "react";
 
 export default function OrderHistory() {
-  return <div>OrderHistory</div>;
+  return (
+    <div className="divide-y">
+      <div className="Order-header libre-baskerville-regular mb-6">
+        ORDER HISTORY
+      </div>
+      <div className="Order-body">
+        <p className="border-solid border-2 border-slate-400 p-5">
+          You haven't placed any orders yet
+        </p>
+      </div>
+    </div>
+  );
 }
