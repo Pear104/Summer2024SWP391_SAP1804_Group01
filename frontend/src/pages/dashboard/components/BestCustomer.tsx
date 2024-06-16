3f20f7699eb3c03173e28ac12b82487bb59f586c
DucDao <minkendy0@gmail.com>
2024-06-16 22:29:30 +0700

    refactor: chart and customer report table

--- frontend/src/pages/dashboard/components/BestCustomer.tsx
+++ frontend/src/pages/dashboard/components/BestCustomer.tsx
@@ -1,108 +1,90 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-
-const recentOrderData = [
-  {
-    id: "1",
-    product_id: "4324",
-    customer_id: "23143",
-    customer_name: "Shirley A. Lape",
-    order_date: "2022-05-17T03:24:00",
-    order_total: "$435.50",
-    current_order_status: "PLACED",
-    shipment_address: "Cottage Grove, OR 97424",
-  },
-  {
-    id: "7",
-    product_id: "7453",
-    customer_id: "96453",
-    customer_name: "Ryan Carroll",
-    order_date: "2022-05-14T05:24:00",
-    order_total: "$96.35",
-    current_order_status: "CONFIRMED",
-    shipment_address: "Los Angeles, CA 90017",
-  },
-  {
-    id: "2",
-    product_id: "5434",
-    customer_id: "65345",
-    customer_name: "Mason Nash",
-    order_date: "2022-05-17T07:14:00",
-    order_total: "$836.44",
-    current_order_status: "SHIPPED",
-    shipment_address: "Westminster, CA 92683",
-  },
-  {
-    id: "3",
-    product_id: "9854",
-    customer_id: "87832",
-    customer_name: "Luke Parkin",
-    order_date: "2022-05-16T12:40:00",
-    order_total: "$334.50",
-    current_order_status: "SHIPPED",
-    shipment_address: "San Mateo, CA 94403",
-  },
-  {
-    id: "4",
-    product_id: "8763",
-    customer_id: "09832",
-    customer_name: "Anthony Fry",
-    order_date: "2022-05-14T03:24:00",
-    order_total: "$876.00",
-    current_order_status: "OUT_FOR_DELIVERY",
-    shipment_address: "San Mateo, CA 94403",
-  },
-  {
-    id: "5",
-    product_id: "5627",
-    customer_id: "97632",
-    customer_name: "Ryan Carroll",
-    order_date: "2022-05-14T05:24:00",
-    order_total: "$96.35",
-    current_order_status: "DELIVERED",
-    shipment_address: "Los Angeles, CA 90017",
-  },
-];
+import { GET } from "../../../utils/request";
 
 export default function RecentOrders() {
+  const [data, setData] = useState<
+    {
+      customerID: string;
+      customerName: string;
+      gender: string;
+      totalOrder: number;
+      totalSpend: number;
+    }[]
+  >([]);
+  useEffect(() => {
+    const fetchData = async () => {
+      const fetchedData = await GET("/api/Accounts");
+      const customerData = fetchedData
+        .filter((account: any) => account?.role === "Customer")
+        .map((account: any) => {
+          const totalSpend = account?.ordersOfCustomer.reduce(
+            (sum: number, order: any) => sum + (order?.totalPrice || 0),
+            0
+          );
+          return {
+            customerID: account?.accountId,
+            customerName: account?.name,
+            gender: account?.gender,
+            totalOrder: account?.ordersOfCustomer.length,
+            totalSpend: totalSpend,
+          };
+        });
+
+      setData(customerData);
+    };
+
+    fetchData();
+  }, []);
+  const sortData = data.sort((a, b) => b.totalSpend - a.totalSpend);
   return (
     <div className="bg-white px-4 pt-3 pb-4 rounded-sm border border-gray-200 flex-1">
       <strong className="text-gray-700 font-medium">Best Customers</strong>
       <div className="border-x border-gray-200 rounded-sm mt-3">
-        <table className="w-full text-gray-700">
+        <table className="w-full text-gray-700 text-left">
           <thead>
             <tr>
-              <th>ID</th>
-              <th>Product ID</th>
-              <th>Customer Name</th>
-              {/* <th>Order Date</th> */}
-              <th>Order Total</th>
-              <th>Shipping Address</th>
-              {/* <th>Order Status</th> */}
+              <th className="px-4 py-2 bg-gray-100 font-semibold">#ID</th>
+              <th className="px-4 py-2 bg-gray-100 font-semibold">
+                Customer Name
+              </th>
+              <th className="px-4 py-2 bg-gray-100 font-semibold">Gender</th>
+              <th className="px-4 py-2 bg-gray-100 font-semibold">
+                Total Order
+              </th>
+              <th className="px-4 py-2 bg-gray-100 font-semibold">
+                Total Spend
+              </th>
             </tr>
           </thead>
           <tbody>
-            {recentOrderData.map((order) => (
-              <tr key={order.id}>
-                <td>
-                  <Link to={`/order/${order.id}`}>#{order.id}</Link>
-                </td>
-                <td>
-                  <Link to={`/product/${order.product_id}`}>
-                    #{order.product_id}
-                  </Link>
-                </td>
-                <td>
-                  <Link to={`/customer/${order.customer_id}`}>
-                    {order.customer_name}
-                  </Link>
-                </td>
-                {/* <td>{format(new Date(order.order_date), 'dd MMM yyyy')}</td> */}
-                <td>{order.order_total}</td>
-                <td>{order.shipment_address}</td>
-                {/* <td>{getOrderStatus(order.current_order_status)}</td> */}
-              </tr>
-            ))}
+            {sortData
+              .filter((customer) => customer.totalSpend > 0)
+              .slice(0, 5)
+              .map((customer) => (
+                <tr key={customer.customerID}>
+                  <td className="px-4 py-2">
+                    {/* <Link to={`/customer/${customer.id}`}>#{customer.id}</Link> */}
+                    #{customer.customerID}
+                  </td>
+                  <td className="px-4 py-2">
+                    {/* <Link to={`/product/${customer.product_id}`}>
+                    #{customer.product_id}
+                  </Link> */}
+                    {customer.customerName}
+                  </td>
+                  <td className="px-4 py-2">
+                    {/* <Link to={`/customer/${customer.customer_id}`}>
+                    {customer.customer_name}
+                  </Link> */}
+                    {customer.gender}
+                  </td>
+                  <td className="px-4 py-2">{customer.totalOrder}</td>
+                  <td className="px-4 py-2">
+                    ${customer.totalSpend.toFixed(2)}
+                  </td>
+                </tr>
+              ))}
           </tbody>
         </table>
       </div>
--- frontend/src/pages/dashboard/components/BuyerProfilePieChart.tsx
+++ frontend/src/pages/dashboard/components/BuyerProfilePieChart.tsx
@@ -1,11 +1,6 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { PieChart, Pie, Cell, ResponsiveContainer, Legend } from "recharts";
-
-const data = [
-  { name: "Male", value: 540 },
-  { name: "Female", value: 620 },
-  { name: "Other", value: 210 },
-];
+import { GET } from "../../../utils/request";
 
 const RADIAN = Math.PI / 180;
 const COLORS = ["#00C49F", "#FFBB28", "#FF8042"];
@@ -43,6 +38,41 @@ const renderCustomizedLabel = ({
 };
 
 export default function BuyerProfilePieChart() {
+  const [data, setData] = useState<{ name: string; value: number }[]>([]);
+  useEffect(() => {
+    (async () => {
+      const fetchedData = await GET("/api/Accounts");
+      let maleCount = 0;
+      let femaleCount = 0;
+      let otherCount = 0;
+
+      // Female
+      //   Male
+      //   Other
+      fetchedData.forEach((account: any) => {
+        if (account?.role == "Customer") {
+          switch (account?.gender) {
+            case "Male":
+              maleCount++;
+              break;
+            case "Female":
+              femaleCount++;
+              break;
+            default:
+              otherCount++;
+              break;
+          }
+        }
+      });
+      const data = [
+        { name: "Male", value: maleCount },
+        { name: "Female", value: femaleCount },
+        { name: "Other", value: otherCount },
+      ];
+      setData(data);
+    })();
+  }, []);
+
   return (
     <div className="w-[20rem] h-[26rem] bg-white p-4 rounded-sm border border-gray-200 flex flex-col">
       <strong className="text-gray-700 font-medium">Buyer Profile</strong>
