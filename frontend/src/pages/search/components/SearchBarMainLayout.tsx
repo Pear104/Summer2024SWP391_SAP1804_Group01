6dae7ab094da2e6d74e2179fa8f9496709e756d7
Dat <datntse180123@fpt.edu.vn>
2024-06-23 09:07:25 +0700

    refactor: update search function

--- frontend/src/pages/search/components/SearchBarMainLayout.tsx
+++ frontend/src/pages/search/components/SearchBarMainLayout.tsx
@@ -1,13 +1,31 @@
+import { Search } from "lucide-react";
+import { useState } from "react";
+
 export default function SearchBarMainLayout() {
+  const [keyword, setKeyword] = useState("");
   return (
-    <div className="bg-gray-100 p-4">
-      <div className="container mx-auto">
+    <div className="border-2 py-1 border-transparent border-slate-200">
+      <form
+        action="./search"
+        method="GET"
+        className="flex justify-end rounded-md overflow-hidden"
+      >
         <input
           type="text"
           placeholder="Search..."
-          className="w-full p-2 border border-gray-300 rounded"
+          name="keyword"
+          className="w-full px-4 outline-none flex-grow"
+          onChange={(e) => {
+            setKeyword(e.target.value);
+          }}
         />
-      </div>
+        <button
+          type="submit"
+          className="flex justify-center items-center rounded-md cursor-pointer px-3"
+        >
+          <Search size={20} absoluteStrokeWidth />
+        </button>
+      </form>
     </div>
   );
 }
