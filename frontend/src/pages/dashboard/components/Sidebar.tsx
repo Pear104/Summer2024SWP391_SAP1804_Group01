251ddea2604546699e739e42e7db28612f536d7f
Võ Đỗ Quang Dương <quangduongaptx4869@gmail.com>
2024-07-12 01:30:21 +0700

    refactor: fix sidebar in dashboard

--- frontend/src/pages/dashboard/components/Sidebar.tsx
+++ frontend/src/pages/dashboard/components/Sidebar.tsx
@@ -1,9 +1,13 @@
-import { Gem, HandCoins, Sparkles, TicketPercent } from "lucide-react";
+import {
+  CircleUser,
+  Gem,
+  HandCoins,
+  Sparkles,
+  TicketPercent,
+} from "lucide-react";
 import { Link, useLocation, useNavigate } from "react-router-dom";
 import classNames from "classnames";
 import {
-  HiOutlineLogout,
-  HiOutlineViewGrid,
   HiOutlineCube,
   HiOutlineShoppingCart,
   HiOutlineUsers,
@@ -11,11 +15,22 @@ import {
   HiCurrencyDollar,
 } from "react-icons/hi";
 import { BsCardText, BsTextParagraph } from "react-icons/bs";
+import { LiaToolsSolid } from "react-icons/lia";
+import { RiVipDiamondLine } from "react-icons/ri";
 import { useQuery } from "@tanstack/react-query";
 import { GET } from "../../../utils/request";
+import React, { useState } from "react";
+import type { MenuProps } from "antd";
+import { Button, Menu } from "antd";
+
+import {
+  MenuFoldOutlined,
+  MenuUnfoldOutlined,
+  PieChartOutlined,
+  TagOutlined,
+} from "@ant-design/icons";
 const linkClasses =
   "flex items-center gap-2 font-light px-3 py-2 md:hover:bg-neutral-100 md:hover:underline hover:bg-neutral-700 hover:no-underline active:bg-neutral-600 round-sm text-base";
-
 type Role =
   | "Manager"
   | "SaleStaff"
@@ -26,39 +41,21 @@ type Role =
 const ROLE_PERMISSIONS: Record<Role, string[]> = {
   Administrator: [
     "dashboard",
-    "orders",
-    "transactions",
-    "warranty-request",
-    "products",
-    "accessories",
-    "customers",
-    "diamond-price",
-    "promotion",
-    "accessory-price",
+    "buying",
+    "warranty",
+    "product",
+    "account",
+    "price",
     "blog",
-    "sales-report",
-    "delivery-report",
-    "potential-customer-report",
-    "warranty-card",
   ],
   Manager: [
     "dashboard",
-    "orders",
-    "transactions",
-    "warranty-request",
-    "products",
-    "accessories",
-    "customers",
-    "diamond-price",
-    "accessory-price",
+    "buying",
+    "warranty",
+    "product",
+    "account",
     "blog",
-    "staffs-report",
-    "price-rate",
-    "promotion",
-    "sales-report",
-    "delivery-report",
-    "potential-customer-report",
-    "warranty-card",
+    "price",
   ],
   SaleStaff: ["dashboard", "salestaff", "warranty-request"],
   // DeliveryStaff: ["dashboard", "deliverystaff", "deliverywarrantystaff"],
@@ -66,21 +63,23 @@ const ROLE_PERMISSIONS: Record<Role, string[]> = {
   WarrantyStaff: ["dashboard", "warranty-request"],
 };
 
+type MenuItem = Required<MenuProps>["items"][number];
+
 export default function Sidebar() {
+  const [collapsed, setCollapsed] = useState(false);
+
+  //function to collapse the sidebar
+  const toggleCollapsed = () => {
+    setCollapsed(!collapsed);
+  };
   const navigate = useNavigate();
   const { data: account } = useQuery({
     queryKey: ["account"],
     queryFn: () => GET("/api/Accounts/me"),
   });
-  // console.log("account: ", account?.name);
-  // console.log(account?.role);
   if (account?.role == "Customer") {
     navigate("/");
   }
-  // if (!account) {
-  //   console.log(account);
-  //   navigate("/authentication/login");
-  // }
   const userRole: Role = account?.role;
 
   const DASHBOARD_SIDEBAR_LINKS = [
@@ -88,7 +87,26 @@ export default function Sidebar() {
       key: "dashboard",
       label: "Dashboard",
       path: "/admin",
-      icon: <HiOutlineViewGrid />,
+      icon: <PieChartOutlined />,
+    },
+    {
+      key: "buying",
+      label: "Buying",
+      icon: <HiOutlineShoppingCart size={20} />,
+      children: [
+        {
+          key: "orders",
+          label: "Orders",
+          path: "/admin/order",
+          icon: <HiOutlineShoppingCart size={20} />,
+        },
+        {
+          key: "transactions",
+          label: "Transactions",
+          path: "/admin/transactions",
+          icon: <HiOutlineDocumentText />,
+        },
+      ],
     },
     {
       key: "orders",
@@ -103,6 +121,25 @@ export default function Sidebar() {
       icon: <HiOutlineDocumentText />,
     },
     {
+      key: "warranty",
+      label: "Warranty",
+      icon: <LiaToolsSolid />,
+      children: [
+        {
+          key: "warranty-request",
+          label: "Warranty Request",
+          path: "/admin/warranty-request",
+          icon: <BsCardText />,
+        },
+        {
+          key: "warranty-card",
+          label: "Warranty Card",
+          path: "/admin/warranty-card",
+          icon: <BsCardText />,
+        },
+      ],
+    },
+    {
       key: "warranty-request",
       label: "Warranty Request",
       path: "/admin/warranty-request",
@@ -113,6 +150,51 @@ export default function Sidebar() {
       label: "Warranty Card",
       path: "/admin/warranty-card",
       icon: <BsCardText />,
+    },
+    {
+      key: "product",
+      label: "Products",
+      icon: <HiOutlineCube />,
+      children: [
+        {
+          key: "diamonds",
+          label: "Diamonds",
+          icon: <RiVipDiamondLine />,
+          children: [
+            {
+              key: "products",
+              label: "Diamonds",
+              path: "/admin/diamonds",
+              icon: <RiVipDiamondLine />,
+            },
+            {
+              key: "diamond-price",
+              label: "Diamond Price",
+              path: "/admin/diamond-price",
+              icon: <HiCurrencyDollar />,
+            },
+          ],
+        },
+        {
+          key: "accessory",
+          label: "Accessories",
+          icon: <Sparkles size={15} />,
+          children: [
+            {
+              key: "accessories",
+              label: "Accessories",
+              path: "/admin/accessories",
+              icon: <Sparkles size={15} />,
+            },
+            {
+              key: "accessory-price",
+              label: "Accessory Price",
+              path: "/admin/accessory-price",
+              icon: <HiCurrencyDollar />,
+            },
+          ],
+        },
+      ],
     },
     {
       key: "products",
@@ -163,6 +245,25 @@ export default function Sidebar() {
       icon: <HiCurrencyDollar />,
     },
     {
+      key: "price",
+      label: "Price",
+      icon: <TagOutlined />,
+      children: [
+        {
+          key: "price-rate",
+          label: "Price Rate",
+          path: "/admin/price-rate",
+          icon: <HandCoins />,
+        },
+        {
+          key: "promotion",
+          label: "Promotion",
+          path: "/admin/promotion",
+          icon: <TicketPercent />,
+        },
+      ],
+    },
+    {
       key: "price-rate",
       label: "Price Rate",
       path: "/admin/price-rate",
@@ -198,14 +299,86 @@ export default function Sidebar() {
       path: "/admin/potential-customer-report",
       icon: <BsTextParagraph />,
     },
+    {
+      key: "account",
+      label: "Account",
+      icon: <CircleUser size={20} />,
+      children: [
+        {
+          key: "customers",
+          label: "Customers",
+          path: "/admin/customers",
+          icon: <HiOutlineUsers />,
+        },
+        {
+          key: "report",
+          label: "Report",
+          icon: <HiOutlineUsers />,
+          children: [
+            {
+              key: "sales-report",
+              label: "Sales staffs report",
+              path: "/admin/sales-report",
+              icon: <BsTextParagraph />,
+            },
+            {
+              key: "delivery-report",
+              label: "Delivery staffs report",
+              path: "/admin/delivery-report",
+              icon: <BsTextParagraph />,
+            },
+            {
+              key: "potential-customer-report",
+              label: "Potential customer",
+              path: "/admin/potential-customer-report",
+              icon: <BsTextParagraph />,
+            },
+          ],
+        },
+      ],
+    },
   ];
 
   const filteredLinks = DASHBOARD_SIDEBAR_LINKS.filter((link) =>
     ROLE_PERMISSIONS[userRole]?.includes(link.key)
   );
 
+  interface DashboardLink {
+    key: string;
+    label: string;
+    path?: string;
+    icon: React.ReactNode;
+    children?: DashboardLink[];
+  }
+
+  const generateMenuItems = (links: DashboardLink[]): MenuItem[] => {
+    return links.map((item) => {
+      if (item.children) {
+        return {
+          key: item.key,
+          icon: item.icon,
+          label: item.label,
+          children: generateMenuItems(item.children),
+          // style: {backgroundColor: '#262626',
+          // },
+        };
+      } else {
+        return {
+          key: item.key,
+          icon: item.icon,
+          label: <Link to={item.path!}>{item.label}</Link>,
+          // style: {backgroundColor: '#262626',
+          //},
+        };
+      }
+    });
+  };
+
+  const items: MenuItem[] = generateMenuItems(filteredLinks);
+
   return (
     <>
+      {/* Reponsive sidebar */}
       <div className="hidden sm:block">
         <div className="flex-1 flex flex-col gap-0.5">
           {filteredLinks.map((items) => (
@@ -213,22 +386,39 @@ export default function Sidebar() {
           ))}
         </div>
       </div>
-      <div className="sm:hidden flex flex-col bg-neutral-900 w-60 p-3 text-white">
-        <Link to={"/"} className="flex items-center gap-2 px-1 py-3">
+      {/* Sidebar */}
+      <div className="sm:hidden flex flex-col bg-neutral-800 w-auto h-auto text-white overflow-y-auto">
+        <Link to={"/"} className="flex items-center gap-2 px-1 py-3 mx-3">
           <Gem strokeWidth={1.75} />
           <div className="text-neutral-100 text-xl">DatJ</div>
         </Link>
+        <Button
+          className="mx-3"
+          type="primary"
+          onClick={toggleCollapsed}
+          style={{ marginBottom: 16, transition: "all 0.5s" }}
+        >
+          {collapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />}
+        </Button>
+        <Menu
+          className="bg-neutral-800 text-neutral-100 items-center p-4  "
+          mode="inline"
+          theme="dark"
+          inlineCollapsed={collapsed}
+          items={items}
+        />
 
-        <div className="flex-1 py-8 flex flex-col gap-0.5">
+        {/* Old sidebar */}
+        {/* <div className="flex-1 py-8 flex flex-col gap-0.5">
           {filteredLinks.map((items) => (
             <SidebarLink key={items.key} items={items} />
           ))}
-        </div>
-        <div className="flex flex-col gap-0.5 pt-2 border-t border-neutral-700">
-          {/* {DASHBOARD_SIDEBAR_BOTTOM_LINKS.map((items) => (
+        </div> */}
+        {/* {DASHBOARD_SIDEBAR_BOTTOM_LINKS.map((items) => (
           <SidebarLink key={items.key} items={items} />
-        ))} */}
+          ))} */}
 
+        {/* <div className="flex flex-col gap-0.5 pt-2 border-t border-neutral-700">
           <Link
             to="/"
             className={classNames(" text-red-500 cursor-pointer", linkClasses)}
@@ -238,7 +428,7 @@ export default function Sidebar() {
             </span>
             Logout
           </Link>
-        </div>
+        </div> */}
       </div>
     </>
   );
